aea5040a53bd79bf9dec944e8e09c10785dbb2e2
dilip <dilipmane101070@gmail.com>
2023-04-18 16:52:14 +0530

    fist commit

---
+++ campaigns/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+# class Compaings(models.Model):
+
+# Content = models.TextField()
+# Campaign_Templates= 
+# Free_Design_Service =
+# Message_Variables = 
+# Archives = 
